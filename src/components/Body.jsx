f5719a0cb0cd0a784219b892b972b208b41d05b0
AyushSingh16 <80613660+AyushSingh16@users.noreply.github.com>
2024-07-26 02:14:27 +0530

    body component for app

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,35 +1,82 @@
 import { restaurantList } from "../constants";
 import RestaurantCard  from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+
+function filterData(searchText, restaurants){
+
+  const filterData = restaurants.filter((restaurant) => restaurant.data.name.includes(searchText));
+  return filterData;
+}
 
 
 const Body = () => {
     // let searchTxt = "KFC";     //one-way data binding
     
     //searchText is a local state variable
-    const[searchInput ,setSearchInput] = useState("KFC");     //To create state variable
+    // destructuring is done here
+
+    const[restaurants,setRestaurants] = useState(restaurantList);
+    const[searchText ,setSearchText] = useState("");     //To create state variable
+
+    useEffect(() => {
+
+      //API call
+      getRestaurants(); 
+
+    },[]);
+
+    
+    async function getRestaurants()
+    { 
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83667");
+      
+      const json = await data.json();
+      console.log(json);
+
+      //optional chaining
+      const fetchedRestaurants = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+      
+      setRestaurants(fetchedRestaurants);
+
+    }
+
+    console.log("render");
 
     return (
-
     <>
     <div className="search-container">
         <input 
           type="text" 
           className="search-input" 
           placeholder="Search" 
-          value={searchInput}
+          value={searchText}
           onChange={(e) => {
-            setSearchInput(e.target.value);
+            //e.target.value => whatever you write in search box input
+            setSearchText(e.target.value); //this will modify my searchInput
         }}
         />
+        <button 
+        className="search-btn"
+        onClick={ () => {
+          //need to filter the search data
+          const data = filterData(searchText, restaurants);
+          //update the state -> restaurants variable
+          setRestaurants(data);
+
+        }}  
+        >
+        Search
+        </button>
     </div>
     
     <div className="restaurant-list">
     {
        //restaurant is a prop, we have given a prop to this component named restaurant whose value is equal to restaurantList's first restaurant
           
-       restaurantList.map((restaurant) => {
-            return <RestaurantCard  {...restaurant.data} key={restaurant.data.id} />;   // similar to {RestaurantCard(restaurantList[0])}i.e. it is a function call only
+
+       restaurants.map((restaurant) => {
+            return <RestaurantCard  {...restaurant.info} key={restaurant.info.id} />;   // similar to {RestaurantCard(restaurantList[0])}i.e. it is a function call only
           })
     }
     </div>
